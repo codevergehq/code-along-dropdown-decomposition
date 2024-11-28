c0d769d8eea21603757df531c04999df3b082bb5
Alexander Schnebel <alexander.schnebel@codeverge.de>
2024-11-28 10:23:00 +0100

    refactor: rename css for select

--- src/components/SelectMonolith.jsx
+++ src/components/SelectMonolith.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import '../styles/Select.css';
+import '../styles/SelectMonolith.css';
 
 const SelectMonolith = ({ items }) => {
     const [isOpen, setIsOpen] = React.useState(false);
